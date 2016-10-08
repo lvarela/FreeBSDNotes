7c6218f841fcdbc29c8926b78d0dce2b5fffbbb2
lvarela <lvarela@dnsgate.net>
2016-10-08 17:37:54 -0400

    Update functions.sh

--- jails/functions.sh
+++ jails/functions.sh
@@ -1,1 +1,16 @@
 
+use_template() {
+  eval "echo \"$(cat $1)\""
+}
+
+#$1= question $2=command to run
+ask() {
+ while true; do
+    read -p "$1 [y/n]?" yn
+    case $yn in
+        [Yy]* ) eval "$2"; break;;
+        [Nn]* ) exit;;
+    * ) echo "Please answer [y]es or [n]o.";;
+    esac
+done
+}
