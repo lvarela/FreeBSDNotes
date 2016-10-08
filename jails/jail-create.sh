703d4bbfb716b8bdf8799ccddbe43bea98b5cc77
lvarela <lvarela@dnsgate.net>
2016-10-08 17:26:02 -0400

    Update jail-create.sh

--- jails/jail-create.sh
+++ jails/jail-create.sh
@@ -8,6 +8,11 @@ jail=$1
 ip=$2
 rel='10.3-RELEASE'
 
+
+use_template() {
+  eval "echo \"$(cat $1)\""
+}
+
 zfs create zroot/jails/$jail
 
 tar -xvf /tmp/base.txz -C /usr/local/jails/$jail
@@ -22,3 +27,14 @@ env UNAME_r=$rel freebsd-update -b /usr/local/jails/$jail IDS
 
 cp /etc/resolv.conf /usr/local/jails/$jail/etc/
 echo hostname=\"$jail\" > /usr/local/jails/$jail/etc/rc.conf
+
+#write to /etc/jail.conf
+jail_name=$1
+jail_ip=$2
+jail_inter=$3
+
+use_template ./jail-create.template >> /etc/jail.conf
+
+
+
+
